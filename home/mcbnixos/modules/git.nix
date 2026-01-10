d161b78dc3000f2da9d1d5258b46e60e7731fa2e
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-10 11:53:27 +0800

    Fix home-manager git config

--- home/mcbnixos/modules/git.nix
+++ home/mcbnixos/modules/git.nix
@@ -4,11 +4,9 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    config = {
-      user = {
-        name = "MCB-SMART-BOY";
-        email = "mcb2720838051@gmail.com";
-      };
+    userName = "MCB-SMART-BOY";
+    userEmail = "mcb2720838051@gmail.com";
+    extraConfig = {
       core = {
         editor = "hx";
         pager = "delta";
@@ -16,7 +14,7 @@
       interactive.diffFilter = "delta --color-only";
       delta = {
         navigate = true;
-        side-by-side = true;
+        "side-by-side" = true;
       };
     };
   };
