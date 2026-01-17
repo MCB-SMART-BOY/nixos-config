0f2cda939c19ace3e1670e761ecbbee37e46b6a9
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-17 14:39:55 +0800

    Fix fcitx5 addons package path

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -16,7 +16,7 @@
         waylandFrontend = true;
         addons =
           (with pkgs; [
-            fcitx5-chinese-addons
+            qt6Packages.fcitx5-chinese-addons
             fcitx5-rime
             fcitx5-gtk
           ])
