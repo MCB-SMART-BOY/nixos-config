dabf24ec754a6416145faf7eac5b92d8bba6e32e
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-25 18:48:58 +0800

    Add extra binary caches

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,6 +7,19 @@
         "nix-command"
         "flakes"
       ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
       max-jobs = 1;
       cores = 4;
       auto-optimise-store = true;
