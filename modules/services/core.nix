8736347bce832dcf6121690aa45911d023157004
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-27 20:46:06 +0800

    Simplify shell vars to avoid Nix interpolation

--- modules/services/core.nix
+++ modules/services/core.nix
@@ -48,8 +48,8 @@ let
           (pkgs.writeShellScript "clash-verge-prestart-${user}" ''
             set -euo pipefail
             uid="$(${pkgs.coreutils}/bin/id -u ${user})"
-            runtime_dir="/run/user/''${uid}/clash-verge-rev"
-            ${pkgs.coreutils}/bin/install -d -m 0700 -o ${user} -g ${user} "''${runtime_dir}"
+            runtime_dir="/run/user/$uid/clash-verge-rev"
+            ${pkgs.coreutils}/bin/install -d -m 0700 -o ${user} -g ${user} "$runtime_dir"
             for dir in \
               "${clashConfig}/clash-verge" \
               "${clashConfig}/clash-verge-rev" \
@@ -57,17 +57,17 @@ let
               "${clashData}/clash-verge-rev" \
               "${clashCache}/clash-verge-rev" \
               "${clashState}/clash-verge-rev"; do
-              ${pkgs.coreutils}/bin/install -d -m 2775 -o ${user} -g ${user} "''${dir}"
+              ${pkgs.coreutils}/bin/install -d -m 2775 -o ${user} -g ${user} "$dir"
             done
             ${pkgs.coreutils}/bin/chown -R ${user}:${user} \
-              "''${clashConfig}/clash-verge" \
-              "''${clashConfig}/clash-verge-rev" \
-              "''${clashData}/clash-verge" \
-              "''${clashData}/clash-verge-rev" \
-              "''${clashCache}/clash-verge-rev" \
-              "''${clashState}/clash-verge-rev" \
+              "${clashConfig}/clash-verge" \
+              "${clashConfig}/clash-verge-rev" \
+              "${clashData}/clash-verge" \
+              "${clashData}/clash-verge-rev" \
+              "${clashCache}/clash-verge-rev" \
+              "${clashState}/clash-verge-rev" \
               2>/dev/null || true
-            rm -f "''${runtime_dir}"/*.sock 2>/dev/null || true
+            rm -f "$runtime_dir"/*.sock 2>/dev/null || true
           '')
         ];
         Environment = [
