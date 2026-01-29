9e0e2ec34ca5cb22d47d182e66af43d7a8ba699c
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-30 02:22:24 +0800

    chore: use stable 25.11 channels

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
   description = "NixOS + Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
